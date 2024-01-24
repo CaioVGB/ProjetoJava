b974c89e99f79864425b7f310253252a8eb84ded
CAIO VINICIUS GUEDES BARBOSA <caio.vgbarbosa@senacsp.edu.br>
2024-01-24 12:06:05 -0300

    Modificadores de Acesso e Criação de Super Class

---
+++ src/pooTurma/Turma.java
@@ -0,0 +1,37 @@
+
+package pooTurma;
+
+
+public class Turma {
+	//Atributos da classe
+	String nome;
+	String telefone;
+	String email;
+	
+	//Construtor
+	public Turma() {
+		System.out.println("_______________");
+		beberAgua();
+	}
+	
+	public void beberAguaAluno() {
+		beberAgua();
+	}
+	//Métodos
+	void ligarPc() {
+		System.out.println("Computador ligado");
+	}
+	
+	void logar() {
+		System.out.println("Login feito com sucesso");
+	}
+		
+	void desligarPc() {
+		System.out.println("Computador desligado");
+	
+	}	
+	
+	private void beberAgua() {
+        	System.out.println("Ir ao bebedouro para beber água");
+	}
+}
